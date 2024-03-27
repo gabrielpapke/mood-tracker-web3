7751fa5f960d94706c588d73bd450422ba332286
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.7>
2024-03-26 23:06:26 -0300

    feat(history): add calendar component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react"
+import { Calendar } from "@/components/ui/calendar"
+import { DateRange } from "react-day-picker"
+
 export function History() {
+  const defaultSelected: DateRange = {
+    from: new Date(),
+    to: new Date(),
+  };
+
+  const [date, setDate] = useState<DateRange | undefined>(defaultSelected)
+
   return (
-    <div>History</div>
+    <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex justify-center items-center space-y-2 h-[70vh]">
+        <Calendar
+          id="calendar"
+          mode="range"
+          defaultMonth={defaultSelected.from}
+          selected={date}
+          className="rounded-md border"
+          onSelect={setDate}
+        />
+      </div>
+    </div>
   )
 }
