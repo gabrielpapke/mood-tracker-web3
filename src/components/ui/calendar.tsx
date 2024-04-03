e67a202f71a2f630bd1b4cae40d29d726723c93c
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.7>
2024-04-03 17:17:53 -0300

    chore(config): add scripts for lint and prettier

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,9 +52,11 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+                IconLeft: ({ ...props }) => (
+                    <ChevronLeft className="h-4 w-4" {...props} />
+                ),
                 IconRight: ({ ...props }) => (
-                    <ChevronRight className="h-4 w-4" />
+                    <ChevronRight className="h-4 w-4" {...props} />
                 ),
             }}
             {...props}
