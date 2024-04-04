f7c1668f615d818a4ed36b78fc6ea66427dc127e
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.7>
2024-04-04 11:07:30 -0300

    test(callendar): first test

--- src/pages/History/components/Calendar/index.spec.tsx
+++ src/pages/History/components/Calendar/index.spec.tsx
@@ -1,3 +1,18 @@
-test('should equals 2', () => {
-    expect(1 + 1).toEqual(2)
+import { render } from '@testing-library/react'
+import { Calendar } from '.'
+
+describe('Calendar', () => {
+    it('should init calendar with today selected', () => {
+        const { container } = render(<Calendar />)
+
+        const today = new Date().getDay()
+
+        const daySelected = container.querySelector(
+            '.day-range-end.rdp-day_range_start',
+        )
+
+        const daySelectedText = Number(daySelected?.innerHTML)
+
+        expect(daySelectedText).toEqual(today)
+    })
 })
